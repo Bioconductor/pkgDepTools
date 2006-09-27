bcf7caefacf04fecd815baa968c0ab6ad7f32957
Seth Falcon <sfalcon@fhcrc.org>
2006-09-27 22:27:14 +0000

    Enable RCurl again, version bump to be included in BioC release
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pkgDepTools@20201 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -3,10 +3,5 @@ globals <- new.env(parent=emptyenv(), hash=TRUE)
 
 .First.lib <- function(libname, pkgname) {
     globals$have_RCurl <- suppressWarnings(require("RCurl", quietly=TRUE))
-    ## FIXME: temporary fix since RCurl segfaults 
-    if (globals$have_RCurl) {
-        cat("Disabling RCurl due to R-devel/RCurl segfault issue\n")
-        globals$have_RCurl <- FALSE
-    }
 }
 .onLoad <- .First.lib

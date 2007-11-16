a5dba7d38061657e5f92b44c02bd349295aa508f
Seth Falcon <sfalcon@fhcrc.org>
2007-11-16 05:23:40 +0000

    Use dosize=FALSE in the vignette
    Obtaining size estimates for packages via RCurl is time intensive
    since a separate HTTP request must be made for each package in the
    repository.  All calls to makeDepGraph in the vignette now specify
    dosize=FALSE to speed processing for the BioC build system.
    
    Output from getInstallOrder is now cleaner when no size information is
    available.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pkgDepTools@28761 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getInstallOrder.R
+++ R/getInstallOrder.R
@@ -4,11 +4,16 @@ getInstallOrder <- function(pkg, depG, needed.only=TRUE) {
       toInst <- setdiff(toInst, rownames(installed.packages()))
     if (length(toInst)) {
         sizes <- unlist(nodeData(depG, n=toInst, attr="size"))
-        sizesC <- as.character(round(sizes, 2))
-        sizesC[is.na(sizes)] <- "?"
-        sizesC <- paste(sizesC, "MB", sep="")
-        names(toInst) <- sizesC
-        total <- sum(sizes[!is.na(sizes)])
+        missingSize <- is.na(sizes)
+        if (!all(missingSize)) {
+            sizesC <- as.character(round(sizes, 2))
+            sizesC[is.na(sizes)] <- "?"
+            sizesC <- paste(sizesC, "MB", sep="")
+            names(toInst) <- sizesC
+            total <- sum(sizes[!is.na(sizes)])
+        } else {
+            total <- as.integer(NA)
+        }
         list(packages=toInst, total.size=total)
     } else {
         list(packages=character(0), total.size=numeric(0))
